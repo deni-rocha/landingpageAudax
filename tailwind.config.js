378b2816678d70dff13f3cc952a826782f42751c
nosli <eu.denilsonrocha@gmail.com>
2022-10-26 10:42:18 -0300

    tailwind config theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
+    colors: {
+      bgDefault: '#0F123F',
+      ...colors
+    },
+    fontFamily: {
+      inter: ['Inter', 'sans-serif'],
+      ...defaultTheme.fontFamily
+    },
     extend: {},
-  },
-  plugins: [],
+    plugins: []
+  }
 }
