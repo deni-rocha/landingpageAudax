afd32a22927e475df4c67fd58433ec5e598d8af1
nosli <eu.denilsonrocha@gmail.com>
2022-10-26 10:56:10 -0300

    component Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,13 @@
 const Header: React.FunctionComponent = () => {
   return (
-    <header className="flex flex-col">
-      <h1 className="text-5xl font-bold">Datanow</h1>
-
-      <nav>
+    <header className="flex absolute gap-2 flex-col md:flex-row w-10/12 h-20 items-center md:justify-between bg-bgDefault border border-white">
+      <h1 className="text-4xl font-extrabold uppercase">Datanow</h1>
+      <nav className="space-x-6 md:space-x-12">
         <a href="">Início</a>
         <a href="">Conheça</a>
         <a href="">Funcionalidades</a>
         <a href="">Contato</a>
       </nav>
-      <p className="self-end">Esse texto é uma headline super legal</p>
     </header>
   )
 }
