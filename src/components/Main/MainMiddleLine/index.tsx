c2db37f8fafd3e536eb119f84f129be7b7165c4e
nosli <eu.denilsonrocha@gmail.com>
2022-10-28 21:29:42 -0300

    Main component modfie images

--- src/components/Main/MainMiddleLine/index.tsx
+++ src/components/Main/MainMiddleLine/index.tsx
@@ -1,5 +1,131 @@
+import Image from 'next/image'
+import firstLeftNotebook from '../../../../public/images/MainMiddleLine/first-left.png'
+import firstRightNotebook from '../../../../public/images/MainMiddleLine/first-right.png'
+import secondRight from '../../../../public/images/MainMiddleLine/second-right.png'
+import thirdLeft from '../../../../public/images/MainMiddleLine/third-left.png'
+import fourthRight from '../../../../public/images/MainMiddleLine/fourth-right.png'
+import fivethLeft from '../../../../public/images/MainMiddleLine/fiveth-left.png'
+import MainCardTopNotebook from './MainCards/MainCardFirstBlock'
+import MainCardLastBlock from './MainCards/MainCardLastBlock'
+
 const MainMiddleLine = () => {
-  return <section>MainMiddleLine</section>
+  return (
+    <section
+      className="py-2 pt-12
+    md:pt-24
+    "
+    >
+      <div
+        className="space-y-2
+      md:flex md:flex-col md:items-center
+      "
+      >
+        <h2
+          className="
+        font-bold text-center
+        md:text-2xl
+        "
+        >
+          Conheça
+        </h2>
+        <p
+          className="text-xs text-center 
+        md:w-10/12
+        "
+        >
+          Ema, também conhecida como nandu, nhandu, guaripé e xuri, é uma ave da
+          família Rheidae nativa da América do Sul. Apesar de possuir grandes
+          asas, é uma ave não voadora.
+        </p>
+      </div>
+
+      <section className="py-12">
+        <div
+          className="space-y-12 flex flex-col 
+        md:flex-row md:items-center p-2 md:justify-center md:gap-6 md:space-y-0
+        "
+        >
+          <MainCardTopNotebook
+            title="99% dos dados adicionados sem"
+            subTitle="fonte não possuem veracidade."
+            description="Inclusive esse texto aqui, que não possui fundo científico algum e é
+        100% errado. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            urlImg={firstLeftNotebook}
+            className="bottom-16"
+          />
+          <MainCardTopNotebook
+            title="Aumento de 24% em vendas"
+            description="Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            urlImg={firstRightNotebook}
+            className="bottom-4"
+          />
+        </div>
+
+        <section className="pt-24 breakpointGrid:grid breakpointGrid:grid-cols-2">
+          <MainCardLastBlock
+            title="Lorem ipsum"
+            subTitle="dolor sit amen"
+            description="Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae ab illo inventore veritatis et quasi."
+            className="md:row-span-1 md:col-span-2"
+          />
+          <MainCardLastBlock className="md:row-span-1 md:col-start-3" isImage>
+            <Image src={secondRight} alt="notebook" objectFit="cover" />
+          </MainCardLastBlock>
+          <MainCardLastBlock
+            title="Lorem ipsum"
+            subTitle="dolor sit amen"
+            description="Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae ab illo inventore veritatis et quasi."
+            className="md:row-span-2 md:col-span-1 "
+          />
+          <MainCardLastBlock className="md:row-start-2 md:col-span-2" isImage>
+            <Image src={thirdLeft} alt="notebook" objectFit="cover" />
+          </MainCardLastBlock>
+          <MainCardLastBlock
+            title="Lorem ipsum"
+            subTitle="dolor sit amen"
+            description="Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae ab illo inventore veritatis et quasi."
+            className="md:row-start-2 md:col-span-1"
+          />
+          <MainCardLastBlock
+            className="md:row-start-3 md:col-start-3 md:order-1"
+            isImage
+          >
+            <Image
+              src={fourthRight}
+              alt="notebook"
+              style={{ background: 'white' }}
+              objectFit="cover"
+            />
+          </MainCardLastBlock>
+          <MainCardLastBlock
+            title="Lorem ipsum"
+            subTitle="dolor sit amen"
+            description="Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae ab illo inventore veritatis et quasi."
+            className="md:row-start-3 md:col-span-2"
+          />
+          <MainCardLastBlock
+            className="md:row-start-4 md:col-span-2 space-y-0 "
+            isImage
+          >
+            <Image
+              src={fivethLeft}
+              alt="notebook"
+              style={{ background: '#6ab1c5' }}
+              objectFit="cover"
+            />
+          </MainCardLastBlock>
+        </section>
+      </section>
+    </section>
+  )
 }
 
 export default MainMiddleLine
--- src/components/Main/MainTopLineMain/index.tsx
+++ src/components/Main/MainTopLineMain/index.tsx
@@ -1,8 +1,8 @@
-import notebookFirst from '../../../assets/MainTopLine/notebook-first.png'
-import dataBL from '../../../assets/MainTopLine/dataBL.png'
-import dataBR from '../../../assets/MainTopLine/dataBR.png'
-import dataTL from '../../../assets/MainTopLine/dataTL.png'
-import dataTR from '../../../assets/MainTopLine/dataTR.png'
+import notebookFirst from '../../../../public/images/MainTopLine/notebook-first.png'
+import dataBL from '../../../../public/images/MainTopLine/dataBL.png'
+import dataBR from '../../../../public/images/MainTopLine/dataBR.png'
+import dataTL from '../../../../public/images/MainTopLine/dataTL.png'
+import dataTR from '../../../../public/images/MainTopLine/dataTR.png'
 import Image from 'next/image'
 
 const MainTopLine = () => {
@@ -11,12 +11,15 @@ const MainTopLine = () => {
       className="
         mt-10 space-y-8 
         xs:space-y-12
-        flex flex-col items-center"
+        flex flex-col items-center
+        md:flex-row md:justify-between
+        "
     >
       <section
         className="
           max-w-xs relative w-full
-          flex flex-col items-center 
+          flex flex-col items-center
+          md:order-1 md:items-end
          "
       >
         <div
@@ -27,36 +30,56 @@ const MainTopLine = () => {
           <Image src={dataTL} alt="table-data" />
         </div>
         <div
-          className="w-10 absolute 
-          top-20 right-8
+          className="
+          w-10 absolute top-20 right-8
+          md:top-36 md:right-5
           "
         >
           <Image src={dataTR} alt="table-data" className="object-cover" />
         </div>
 
-        <div className="h-60 w-64 z-10">
+        <div
+          className="
+        h-60 w-64 z-10           
+        md:h-72 md:w-80
+        "
+        >
           <Image src={notebookFirst} alt="notebook" className="object-cover" />
         </div>
 
-        <div className="w-14 absolute bottom-3 left-4">
+        <div
+          className="
+        w-14 absolute bottom-3 left-4
+        "
+        >
           <Image src={dataBL} alt="table-data" className="object-cover" />
         </div>
-        <div className="w-16 absolute bottom-6 right-10">
+        <div
+          className="
+        w-16 absolute bottom-6 right-10
+        md:w-24 md:-bottom-3 md:right-4
+        "
+        >
           <Image src={dataBR} alt="table-data" className="object-cover" />
         </div>
       </section>
 
-      <div className="p-2">
+      <div className="p-2 w-96 lg:mr-4 ">
         <section
           className="
-            font-bold leading-6 text-lg
-            xs:text-md
+            font-bold leading-6 
+            md:text-xl
+            lg:text-2xl
             "
         >
           <h1 className="">Esse texto é uma</h1>
           <h1 className="">headline super legal</h1>
         </section>
-        <p className="text-xs pt-2 text-textSecondary">
+        <p
+          className="text-xs pt-2 text-textSecondary
+          
+        "
+        >
           E esse texto aqui acompanha o título. Já que eu preciso falar muito,
           vou falar sobre Emas. Considerada a maior ave do continente americano,
           a ema pode chegar a medir 1,70 m de altura! Impressionante, não é?
@@ -65,11 +88,13 @@ const MainTopLine = () => {
         <div
           className="
             w-full text-center pt-4
+            md:text-left
             "
         >
           <button
             className="
-            h-10 w-72 rounded-full font-bold contrast-125 bg-btnColorGreen
+            h-8 w-60 rounded-full font-bold contrast-150 bg-btnColorGreen
+            text-sm
             xs:h-8 xs:w-60
             "
           >
